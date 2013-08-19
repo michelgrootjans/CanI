fe9cfe5deb18a5a2f4ef106b4903de6eb329ef1d
Michel Grootjans <michel.grootjans@gmail.com>
2013-08-19 09:16:57 +0200

    Created Ability

--- src/CanI.Demo/App_Start/FilterConfig.cs
+++ src/CanI.Demo/App_Start/FilterConfig.cs
@@ -8,7 +8,20 @@ namespace CanI.Demo
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeWithCanIAttribute());
+            filters.Add(new AuthorizeWithCanIAttribute(new DemoAbility()));
+        }
+    }
+
+    public class DemoAbility : IMvcAbility
+    {
+        public ActionResult OnAuthorizationFailed()
+        {
+            return new RedirectResult("/");
+        }
+
+        public bool Can(string action, string controller)
+        {
+            return action == "Index" && controller == "Home";
         }
     }
 }
--- src/CanI.Demo/Controllers/AuthorizeWithCanIAttribute.cs
+++ src/CanI.Demo/Controllers/AuthorizeWithCanIAttribute.cs
@@ -2,17 +2,34 @@
 
 namespace CanI.Demo.Controllers
 {
+    // Warning: this class will be a singleton for the whole application !!
     public class AuthorizeWithCanIAttribute : IAuthorizationFilter
     {
+        private readonly IMvcAbility configuration;
+
+        public AuthorizeWithCanIAttribute(IMvcAbility configuration)
+        {
+            this.configuration = configuration;
+        }
 
         public void OnAuthorization(AuthorizationContext filterContext)
         {
             var controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
             var action = filterContext.ActionDescriptor.ActionName;
 
-            if (controller == "Home" && action == "Index") return;
+            if (configuration.Can(action, controller)) return;
 
-            filterContext.Result = new RedirectResult("/");
+            filterContext.Result = configuration.OnAuthorizationFailed();
         }
+    }
+
+    public interface IMvcAbility : IAbility
+    {
+        ActionResult OnAuthorizationFailed();
+    }
+
+    public interface IAbility
+    {
+        bool Can(string action, string controller);
     }
 }
