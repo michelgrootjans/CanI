37146e8b74f1f9449b0156560bef9e6f238b8751
Michel Grootjans <michel.grootjans@gmail.com>
2013-09-01 12:42:54 +0200

    extracted action cleaner

--- src/CanI.Core/Ability.cs
+++ src/CanI.Core/Ability.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CanI.Core
@@ -7,26 +8,17 @@ namespace CanI.Core
     {
         private readonly List<Permission> permissions;
         private readonly IList<string> ignoredSubjectPostfixes;
-        private readonly IDictionary<string, string> actionAliases;
         private readonly IDictionary<string, string> subjectAliases;
+        private readonly SubjectCleaner subjectCleaner;
+        private ActionCleaner actionCleaner;
 
         public Ability()
         {
             permissions = new List<Permission>();
             ignoredSubjectPostfixes = new List<string>();
+            actionCleaner = new ActionCleaner();
+            subjectCleaner = new SubjectCleaner();
             subjectAliases = new Dictionary<string, string>();
-            actionAliases = new Dictionary<string, string>
-            {
-                {"index", "view"},
-                {"show", "view"},
-                {"detail", "view"},
-                {"read", "view"},
-                {"insert", "create"},
-                {"update", "edit"},
-                {"change", "edit"},
-                {"remove", "delete"},
-                {"destroy", "delete"}
-            };
         }
 
         public bool Allows(string action, object subject)
@@ -59,8 +51,7 @@ namespace CanI.Core
 
         public void ConfigureActionAlias(string intendedAction, params string[] aliases)
         {
-            foreach (var alias in aliases)
-                actionAliases.Add(alias, intendedAction);
+            actionCleaner.ConfigureAliases(intendedAction, aliases);
         }
 
         public void ConfigureSubjectAliases(string intendedSubject, params string[] aliases)
@@ -73,10 +64,7 @@ namespace CanI.Core
 
         private string CleanupAction(string action)
         {
-            var lowerCaseAction = action.ToLower();
-            if (actionAliases.ContainsKey(lowerCaseAction))
-                return actionAliases[lowerCaseAction];
-            return lowerCaseAction;
+            return actionCleaner.Clean(action);
         }
 
         private string CleanupSubject(object subject)
@@ -108,5 +96,44 @@ namespace CanI.Core
                 subject = subjectAliases[subject];
             return subject;
         }
+    }
+
+    public class ActionCleaner
+    {
+        private readonly IDictionary<string, string> actionAliases;
+
+        public ActionCleaner()
+        {
+            actionAliases = new Dictionary<string, string>
+            {
+                {"index", "view"},
+                {"show", "view"},
+                {"detail", "view"},
+                {"read", "view"},
+                {"insert", "create"},
+                {"update", "edit"},
+                {"change", "edit"},
+                {"remove", "delete"},
+                {"destroy", "delete"}
+            };
+        }
+
+        public string Clean(string action)
+        {
+            var lowerCaseAction = action.ToLower();
+            if (actionAliases.ContainsKey(lowerCaseAction))
+                return actionAliases[lowerCaseAction];
+            return lowerCaseAction;
+        }
+
+        public void ConfigureAliases(string intendedAction, string[] aliases)
+        {
+            foreach (var alias in aliases)
+                actionAliases.Add(alias, intendedAction);
+        }
+    }
+
+    public class SubjectCleaner
+    {
     }
 }
---
+++ src/CanI.Core/ActionCleaner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CanI.Core
+{
+    public class ActionCleaner
+    {
+        private readonly IDictionary<string, string> actionAliases;
+
+        public ActionCleaner()
+        {
+            actionAliases = new Dictionary<string, string>
+            {
+                {"index", "view"},
+                {"show", "view"},
+                {"detail", "view"},
+                {"read", "view"},
+                {"insert", "create"},
+                {"update", "edit"},
+                {"change", "edit"},
+                {"remove", "delete"},
+                {"destroy", "delete"}
+            };
+        }
+
+        public string Clean(string action)
+        {
+            var lowerCaseAction = action.ToLower();
+            if (actionAliases.ContainsKey(lowerCaseAction))
+                return actionAliases[lowerCaseAction];
+            return lowerCaseAction;
+        }
+
+        public void ConfigureAliases(string intendedAction, string[] aliases)
+        {
+            foreach (var alias in aliases)
+                actionAliases.Add(alias, intendedAction);
+        }
+    }
+}
